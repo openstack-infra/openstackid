4b2a8b9c42a8dc8d66e25eeb1f190d578a3644df
Sebastian Marcet <smarcet@gmail.com>
2015-07-03 17:43:14 -0300

    OIDC - OpenId Connect Implementation
    DB refactoring
    Client Admin Rectoring
    upgraded layout to use latest bootstrap
    Added bower support
    Added Behat support
    OIDC Discovery suuport added
    OIDC JWKS endpoint added
    Refactored OpenId workflows
    Refactored OAuth2 workflows
    Server Keys Admin Added
    Authorization Code Flow refactored to support OIDC
    Allow native apps to use auth code grant
    Allow native apps to use "TokenEndpoint_AuthMethod_PrivateKeyJwt"
    Filter on UI public/private keys algs based on the key usage
    Set as default auth protocol for private clients "client_secret_basic"
    Added feature client_secret_expired
    Filtered content of Token Endpoint Authorization Signed Algorithm based on Token Endpoint Authorization Method
    Implemented OAuth 2.0 Multiple Response Type Encoding Practices
    Implemented OAuth 2.0 Form Post Response Mode
    Implicit Flow refactored to support OIDC
    UserInfo Endpoint (OIDC/Claims)
    Hybrid Flow
    OIDC Session Management
    * WIP - bug fixing
    
    Change-Id: If3d38666f3f7f56bd8c94b9df2e6340554512612

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     @yield('title')
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{ HTML::style('css/bootstrap.min.css') }}
-    {{ HTML::style('css/main.css') }}
-    {{ HTML::style('css/smoothness/jquery-ui-1.10.3.custom.min.css') }}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{ HTML::style('bower_assets/bootstrap/dist/css/bootstrap.min.css') }}
+    {{ HTML::style('assets/css/main.css') }}
+    {{ HTML::style('bower_assets/jquery-ui/themes/ui-darkness/jquery-ui.css') }}
+    {{ HTML::style('bower_assets/fontawesome/css/font-awesome.min.css') }}
+    @yield('css')
 </head>
 <body>
     <div class="container">
-        <header class="row-fluid">
-            <div class="span5">
+        <header class="row header">
+            <div class="col-md-5">
                 <h1 id="logo"><a href="/">Open Stack</a></h1>
             </div>
-            <div class="span7">
+            <div class="col-md-7">
                 @yield('header_right')
             </div>
         </header>
-        <div class="row-fluid" id="main-content">
+        <div class="row" id="main-content">
             @yield('content')
         </div>
-        <footer class="row-fluid"></footer>
+        <footer class="row"></footer>
     </div>
-
-    {{ HTML::script('js/jquery-2.1.0.min.js')}}
-    {{ HTML::script('js/bootstrap.min.js')}}
-    {{ HTML::script('js/pure.min.js')}}
-    {{ HTML::script('js/jquery.validate.min.js')}}
-    {{ HTML::script('js/additional-methods.min.js')}}
-    {{ HTML::script('js/ajax.utils.js')}}
-    {{ HTML::script('js/jquery.cleanform.js')}}
-    {{ HTML::script('js/jquery.serialize.js')}}
-    {{ HTML::script('js/jquery.validate.additional.custom.methods.js')}}
+    {{ HTML::script('bower_assets/jquery/dist/jquery.min.js')}}
+    {{ HTML::script('bower_assets/bootstrap/dist/js/bootstrap.min.js')}}
+    {{ HTML::script('bower_assets/jquery-validate/dist/jquery.validate.min.js')}}
+    {{ HTML::script('bower_assets/jquery-validate/dist/additional-methods.min.js')}}
+    {{ HTML::script('bower_assets/pure-templates/libs/pure.min.js')}}
+    {{ HTML::script('bower_assets/uri.js/src/URI.min.js')}}
+    {{ HTML::script('assets/js/ajax.utils.js')}}
+    {{ HTML::script('assets/js/jquery.cleanform.js')}}
+    {{ HTML::script('assets/js/jquery.serialize.js')}}
+    {{ HTML::script('assets/js/jquery.validate.additional.custom.methods.js')}}
     @yield('scripts')
 </body>
 </html>
