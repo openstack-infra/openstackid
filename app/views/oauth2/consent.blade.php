88dd2c708a8e9e77ed55bcd752161a8532d9173c
Sebastian Marcet <smarcet@gmail.com>
2016-03-23 13:40:32 -0300

    Updated OAUTH2.0 consent UI
    updated consent UI to be more mobile friendly
    
    Change-Id: I73a8388f49f716bd90ed79131d4b3e38da06428d

--- app/views/oauth2/consent.blade.php
+++ app/views/oauth2/consent.blade.php
@@ -18,11 +18,8 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <div class="row">
-                <div class="col-md-3">
-                    <a target='_blank' href="{{$website}}"><img src="{{$app_logo}}"  class="img-rounded" border="0" width="150"  height="150"/></a>
-                </div>
-                <div class="col-md-9">
-                    <h2>{{$app_name}}&nbsp; <span class="glyphicon glyphicon-info-sign pointable info" aria-hidden="true" data-content="Developer Email: <a href='mailto:{{$dev_info_email}}'>{{$dev_info_email}}</a>.<br> Clicking 'Accept' will redirect you to: <a href='{{$website}}' target='_blank'>{{$website}}</a>" title="Developer Info"></span></h2>
+                <div class="col-md-12 col-xs-12">
+                    <h2> <a target='_blank' href="{{$website}}">{{$app_name}}</a>&nbsp; <span class="glyphicon glyphicon-info-sign pointable info" aria-hidden="true" data-content="Developer Email: <a href='mailto:{{$dev_info_email}}'>{{$dev_info_email}}</a>.<br> Clicking 'Accept' will redirect you to: <a href='{{$website}}' target='_blank'>{{$website}}</a>" title="Developer Info"></span></h2>
                 </div>
             </div>
             <legend>This app would like to:</legend>
@@ -32,7 +29,7 @@
             @endforeach
             </ul>
             <p class="privacy-policy">
-                ** <b>{{$app_name}}</b> Application and <b>Openstack</b> will use this information in accordance with their respective <a target='_blank' href="{{$tos_uri}}">terms of service</a> and <a target='_blank' href="{{$policy_uri}}">privacy policies</a>.
+                ** <b>{{$app_name}}</b> Application and <b>OpenStack</b> will use this information in accordance with their respective <a target='_blank' href="{{$tos_uri}}">terms of service</a> and <a target='_blank' href="{{$policy_uri}}">privacy policies</a>.
             </p>
             {{ Form::open(array('url' => URL::action("UserController@postConsent") ,'id'=>'authorization_form', 'method' => 'post',  "autocomplete" => "off")) }}
                 <input type="hidden"  name='trust' id='trust' value=""/>
