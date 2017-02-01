dff98a009ae00285b98b2d00121ebbf43d983318
Sebastian Marcet <smarcet@gmail.com>
2017-02-01 17:05:36 -0300

    Fixed user name generation in the past user name generation was no trimming white spaces, so was adding %20 on openid url. Also added migration to fix the former user names.
    Change-Id: Ic784102433def9b47b9151800623eea2f32920b2

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,7 +4,7 @@ use Auth\User;
 use Models\Member;
 use OpenId\Services\OpenIdServiceCatalog;
 use Illuminate\Support\Facades\App;
-
+use Auth\UserNameGeneratorService;
 /**
  * Class UserTest
  */
@@ -25,5 +25,18 @@ class UserTest extends TestCase
         $user = User::where('identifier','=','sebastian.marcet')->first();
         $service = App::make(OpenIdServiceCatalog::UserService);
         $service->lockUser($user->id);
+    }
+
+    public function testUserNameGeneration(){
+        $generator = new UserNameGeneratorService();
+        $member6 = Member::findOrFail(6);
+        $member7 = Member::findOrFail(7);
+        $member8 = Member::findOrFail(8);
+        $id6 = $generator->generate($member6);
+        $this->assertTrue( $id6 == 'bharath.kumar.m.r');
+        $id7 = $generator->generate($member7);
+        $this->assertTrue( $id7 == 'yuanying');
+        $id8 = $generator->generate($member8);
+        $this->assertTrue( $id8 == 'sebastian.german.marcet.gomez');
     }
 }
