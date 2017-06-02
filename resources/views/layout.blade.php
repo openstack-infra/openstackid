f2e6196714f5acbd609be0a494febe94e44f9f5f
Sebastian Marcet <smarcet@gmail.com>
2017-06-02 16:05:36 -0300

    Added Favorite icon to main template
    Change-Id: Icb24537e73f2ed4b6f25135d8a6f7fa77a1b6786

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,11 @@
 <head>
     @yield('title')
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
     @yield('meta')
     {!! HTML::style('bower_assets/bootstrap/dist/css/bootstrap.min.css') !!}
     {!! HTML::style('assets/css/main.css') !!}
